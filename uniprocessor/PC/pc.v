1fb79566263b979f2cde88572f4e9fa745b8ecc6
tsengs0 <tsengs0@gmail.com>
2018-11-27 22:07:51 +0900

    Complete the implementation of program counter (PC)

--- uniprocessor/PC/pc.v
+++ uniprocessor/PC/pc.v
@@ -3,16 +3,23 @@ module pc {
 	input wire sys_clk,
 	input wire rstn,
 
-	output reg [5:0] pc_addr, // Point the memory address of current instruction inside the instruction memory (32-bit instruction)
+	output reg [`InstAddrBus] pc_addr, // Point the memory address of current instruction inside the instruction memory (32-bit instruction)
 	output reg	 ce // Enable the instruction memory to be accessable 
 };
 
 	always@ (posedge sys_clk, rtsn) 
 	begin
-		if(rstn == 1'b0) ce <= 1'b0;
-		else		 ce <= 1'b1;
+		if(rstn == `RstEnable) ce <= `ChipDisable;
+		else		       ce <= `ChipEnable;
 	end
 
 	always@ (posedge sys_clk, rstn)
+	begin
+		if(rstn == `RstEnable && ce == `ChipDisable ) begin
+			pc_addr[`InstAddrBus] <= `InstAddrWidth'd0;
+		else
+			pc_addr[`InstAddrBus] <= pc_addr[`InstAddrBus] + 4'h4
+		end	
+	end
 
 endmodule
